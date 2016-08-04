3a564ce27e1b1c4a0f42df3497bfe41e386b4f19
Premek Maly <premma@gmail.com>
2016-08-04 14:59:50 +0200

    new version of NodeJS and npm

--- protractor-headless/protractor.sh
+++ protractor-headless/protractor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-CHROME_DEVEL_SANDBOX="" xvfb-run --server-args='-screen 0 1280x1024x24' protractor $@
+cd "$1"
+CHROME_DEVEL_SANDBOX="" xvfb-run --server-args='-screen 0 1280x1024x24' protractor $2
 
